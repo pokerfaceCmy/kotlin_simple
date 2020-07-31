030c4b479495f0c9459d0c27f3d5b15320656e84
pokerfaceCmy <cheng.meng.yuan@qq.com>
2020-07-31 18:02:17 +0800

    实现部分HttpManager代码

--- simple/app/src/main/java/com/cmy/kotlinsimple/MainActivity.kt
+++ simple/app/src/main/java/com/cmy/kotlinsimple/MainActivity.kt
@@ -2,13 +2,24 @@ package com.cmy.kotlinsimple
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.blankj.utilcode.util.LogUtils
 import com.cmy.common.repository.LoadingDialog
+import com.cmy.common.repository.remote.HttpManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var s: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val http: HttpManager = HttpManager.Builder().setBaseUrl("https://gank.io").build()
+        LogUtils.d(http.mBaseUrl)
+        LogUtils.d(http.mDefaultErrorMsg)
+        LogUtils.d(::s.isLateinit)
+        s = "1"
+        LogUtils.d(::s.isLateinit)
         button.setOnClickListener {
 //            val ratingDialog = RatingDialog(this)
 //            ratingDialog.show()
--- simple/common/src/main/java/com/cmy/common/repository/remote/HttpManager.kt
+++ simple/common/src/main/java/com/cmy/common/repository/remote/HttpManager.kt
@@ -1,12 +1,13 @@
 package com.cmy.common.repository.remote
 
-import com.blankj.utilcode.util.LogUtils
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.ObservableTransformer
 import io.reactivex.rxjava3.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 
 
@@ -16,48 +17,81 @@ import java.util.concurrent.TimeUnit
  * @description 网络数据请求
  * @email cheng.meng.yuan@qq.com
  */
-class HttpManager private constructor() {
-    private lateinit var mRetrofitBuilder: Retrofit.Builder
-    private lateinit var mOkHttpBuilder: OkHttpClient.Builder
-    private lateinit var mRetrofit: Retrofit
+open class HttpManager private constructor(
+    var mBaseUrl: String,
+    var mDefaultErrorMsg: String,
+    var mOkHttpBuilder: OkHttpClient.Builder,
+    var mRetrofitBuilder: Retrofit.Builder
+) {
 
-    // TODO: 2020/7/30 获取BaseUrl,DefaultErrorMsg,tag
+    private constructor(builder: Builder) : this(
+        builder.baseUrl,
+        builder.defaultErrorMsg,
+        builder.okHttpBuilder,
+        builder.retrofitBuilder
+    )
 
-    //静态内部类实现线程安全的单例模式
-    companion object {
-        val instance = SingletonHolder.holder
+
+    class Builder {
+        lateinit var baseUrl: String
+            private set
+        lateinit var defaultErrorMsg: String
+            private set
+        lateinit var okHttpBuilder: OkHttpClient.Builder
+            private set
+        lateinit var retrofitBuilder: Retrofit.Builder
+            private set
+
+        fun setBaseUrl(baseUrl: String) = apply { this.baseUrl = baseUrl }
+        fun setDefaultErrorMsg(defaultErrorMsg: String) =
+            apply { this.defaultErrorMsg = defaultErrorMsg }
+
+        fun setOkHttpBuilder(okHttpBuilder: OkHttpClient.Builder) =
+            apply { this.okHttpBuilder = okHttpBuilder }
+
+        fun setRetrofitBuilder(retrofitBuilder: Retrofit.Builder) =
+            apply { this.retrofitBuilder = retrofitBuilder }
+
+        fun build(): HttpManager {
+            return HttpManager(
+                baseUrl,
+                if (!::defaultErrorMsg.isLateinit) defaultErrorMsg else "服务器开小差啦",
+                if (!::okHttpBuilder.isLateinit) okHttpBuilder else generateOkHttpBuilder(),
+                if (!::retrofitBuilder.isLateinit) retrofitBuilder else generateRetrofitBuilder(
+                    baseUrl
+                )
+            )
+        }
+
+        /**
+         * 创建OkHttpBuilder
+         */
+        private fun generateOkHttpBuilder(): OkHttpClient.Builder {
+            return OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS)
+        }
+
+        /**
+         * 创建RetrofitBuilder
+         */
+        private fun generateRetrofitBuilder(baseUrl: String): Retrofit.Builder {
+            return Retrofit.Builder().baseUrl(baseUrl)
+        }
     }
 
-    private object SingletonHolder {
-        val holder = HttpManager()
-    }
-
-
-    /**
-     * 创建OkHttpBuilder
-     */
-    private fun generateOkHttpBuilder(): OkHttpClient.Builder {
-        mOkHttpBuilder = OkHttpClient.Builder()
-            .connectTimeout(10, TimeUnit.SECONDS)
-            .readTimeout(10, TimeUnit.SECONDS)
-            .writeTimeout(10, TimeUnit.SECONDS)
-        LogUtils.dTag("mOkHttpBuilder的hash值", mOkHttpBuilder.hashCode())
-        return mOkHttpBuilder
-
-    }
-
-    /**
-     * 创建RetrofitBuilder
-     */
-    private fun generateRetrofitBuilder(): Retrofit.Builder {
-        mRetrofitBuilder = Retrofit.Builder()
-
-        LogUtils.dTag("mRetrofitBuilder的hash值", mRetrofitBuilder.hashCode())
-        return mRetrofitBuilder
+    fun getUrl(): String {
+        return mBaseUrl
     }
 
     fun <T> create(cls: Class<T>): T {
-        return generateRetrofitBuilder().build().create(cls)
+        return mRetrofitBuilder
+            .client(mOkHttpBuilder.build())
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .build()
+            .create(cls)
     }
 
     fun <T> composeRequest(observable: Observable<T>): Observable<T> {
